498740c2d8a6fefa3f16d54ea87471bc0cfe1403
Jaime Oyarzún Knittel <mito.oyarzun@gmail.com>
2018-10-25 00:50:33 -0700

    Updates

---
+++ precache-manifest.5bb65fddb7109d86119431e0e72fcb55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd68bd654f3e50b2b2df",
+    "url": "/i94-date-calculator/static/js/runtime~main.cd68bd65.js"
+  },
+  {
+    "revision": "1bc857a80e0adc373086",
+    "url": "/i94-date-calculator/static/js/main.1bc857a8.chunk.js"
+  },
+  {
+    "revision": "88addcd89fae5eb897b5",
+    "url": "/i94-date-calculator/static/js/1.88addcd8.chunk.js"
+  },
+  {
+    "revision": "1bc857a80e0adc373086",
+    "url": "/i94-date-calculator/static/css/main.0ef8aa76.chunk.css"
+  },
+  {
+    "revision": "b7c7be98b20bd47091b250d03cb0c81f",
+    "url": "/i94-date-calculator/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/i94-date-calculator/precache-manifest.5bb65fddb7109d86119431e0e72fcb55.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/i94-date-calculator/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
